b2871b8c058fd98bd9ecc3c858cee9393a655663
Yangthirteen <1785054901@qq.com>
2019-08-29 15:00:38 +0800

    配置问题

--- src/main/java/com/lqc/dao/AdminDao.java
+++ src/main/java/com/lqc/dao/AdminDao.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Select;
 import com.lqc.entity.Admin;
 
 public interface AdminDao {
+	//使用注解方式使用数据库
 	@Insert("insert into admin values(null,#{aname},#{amobile},#{apwd})")
 	public int addAdmin(Admin admin);
 	@Select("select * from admin where amobile=#{amobile} and apwd=#{apwd}")
